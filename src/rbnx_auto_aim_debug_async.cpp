bf503deae3c0ef46319a927b350d30c8ed39a157
cyn <m18876416377@163.com>
2026-07-25 21:15:26 +0800

    正常编译

--- src/rbnx_auto_aim_debug_async.cpp
+++ src/rbnx_auto_aim_debug_async.cpp
@@ -189,10 +189,10 @@ int main(int argc, char * argv[])
       tools::draw_points(img, image_points, {0, 0, 255});
     }
 
-    cv::resize(img, img, {}, 0.5, 0.5);
-    cv::imshow("reprojection", img);
-    auto key = cv::waitKey(1);
-    if (key == 'q') break;
+    // cv::resize(img, img, {}, 0.5, 0.5);
+    // cv::imshow("reprojection", img);
+    // auto key = cv::waitKey(1);
+    // if (key == 'q') break;
   }
 
   quit = true;
