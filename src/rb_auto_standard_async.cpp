e158dcb913e8c608e981ae123328e35c295b1ce0
kokonoka9days <3303133305@qq.com>
2026-08-02 01:07:10 +0800

    暂存

--- src/rb_auto_standard_async.cpp
+++ src/rb_auto_standard_async.cpp
@@ -32,7 +32,7 @@ using namespace tools;
 
 const std::string keys =
   "{help h usage ? |                        | 输出命令行参数说明}"
-  "{@config-path   | ../configs/xiaohei.yaml | 位置参数，yaml配置文件路径 }";
+  "{@config-path   | ../configs/mouse.yaml | 位置参数，yaml配置文件路径 }";
 
 int main(int argc, char * argv[])
 {
